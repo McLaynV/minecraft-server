f8103c115b573d3464b0e8eca0b666ae704deb01
McLayn <mclayn.v@gmail.com>
2023-09-28 18:14:47 +0200

    fix cron backup

--- backup.sh
+++ backup.sh
@@ -2,11 +2,16 @@
 
 sleep 5 # let the server finish
 
+cd "$(dirname "${0}")"
+
 message="$(date +'%F %T') ${*}"
 echo "${message}"
 
 git add .
+echo "Add exited ${?}"
 git commit -a -m "${message}"
+echo "Commit exited ${?}"
 git push
+echo "push exited ${?}"
 
 echo ""
